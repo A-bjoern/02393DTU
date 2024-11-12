f65888e5d1b5e5a7cc787de44692c2b7c6ab9fdc
Asbjørn <s204026@student.dtu.dk>
2024-11-12 18:29:26 +0100

    finished Fibonacci with recursion. Bit primitive, but it works.

--- ex09/Fibonacci.cpp
+++ ex09/Fibonacci.cpp
@@ -1,7 +1,33 @@
 //
 // Created by asbpo on 05-11-2024.
 //
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void Fibonacci(int n, int f1=1, int f2=0, int counter=0){
+    int f3 = f1+f2;
+    if(n <= 1){
+        cout << f3;
+        return;
+    }
+    if(counter == n){
+        cout << f3;
+        return;
+    }
+    Fibonacci(n,f2,f3,counter+1);
+}
+
 
 int main(){
-
+    int n;
+    while(true) {
+        std::cin >> n;
+        if (std::cin.fail()) break;
+        Fibonacci(n);
+        cout << " ";
+    }
+    cout << endl;
+    return 0;
 }
